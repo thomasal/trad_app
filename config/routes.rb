27d736a30fe9feb16b665eddd6f5e9f5f194965b
thomasal <alexandre-01@orange.fr>
2016-02-21 22:44:46 +0100

    qd

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :transactions
   resources :traders
   resources :equities
-  resources :equities
+
+  resources :traders do
+    resources :transactions
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
